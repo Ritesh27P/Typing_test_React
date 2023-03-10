5c50aa95d1b6cef03440ae6b7f5fac307b7343f8
ritesh-blog27 <riteshminecraft27@gmail.com>
2023-03-10 19:40:54 +0530

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import Text from './Text';
-
+import React from 'react';
 
 const App = ()=>{
+
+    let [time, setTime] = useState(0);
+    let [isTimerOn, setIsTimerOn] = useState(true);
+    let [nIntervalId, setnIntervalId] = useState('');
+
+    function writingStopped(event){
+        if(isTimerOn){
+            setIsTimerOn(false);
+            nIntervalId = setInterval(()=>{
+                setTime((prevTime)=>{
+                    return prevTime + 1;
+                })
+            }, 1000)
+        }
+    }
+
+
     return <div>
-        <Text />
+        <h1 className='time'>{time}</h1>
+        <Text time={time} st_func={writingStopped} intervalId={nIntervalId}/>
     </div>
 }
 
--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
 import data from "../text_data"
 
+// Make 5 errors in typing and game over
 
-const Text = ()=>{
-    let [correctChar, setCorrectChar] = useState(true)
+const Text = (props)=>{
     let [sentenceArr, setSentenceArr] = useState(data.split(" "))
     let [sentence, setSentence] = useState(data)
-    let speacial_key = [190, 191, 222, 188]
+    let [unaccuracy, setUnaccuracy] = useState(0)
 
 
     // Handing key press
@@ -17,31 +17,40 @@ const Text = ()=>{
         let Message_list = Message.split(" ")
         let last_word = Message_list.at(-1)
 
+
+        props.st_func()
+
         if (keyPress === sentence[0]){
             setSentence(prevData=>{
                 return prevData.slice(1);
             })
-            setCorrectChar(true)
         } else {
-            setCorrectChar(false)
+            event.target.value = "  "
+            setUnaccuracy(prevData=>prevData+1)
+            console.log(unaccuracy)
         }
 
         // If pressed space bar 
         if (event.keyCode === 32){
+            event.target.value = " "
             if (last_word === sentenceArr[0]){
-                console.log("Same word");
                 setSentenceArr(prevData=>{
                     prevData.shift()
                     return prevData;
                 })
             }
         }
+
+        if (props.time > 5) {
+            clearInterval(props.nIntervalId);
+            
+        }
     }
 
     return <div>
                 <div className="row textF">
-                    <input className="col-6" onKeyDown={handle_key} placeholder="Click here"/>
-                    <p className="col-6" style={{marginTop: "75px"}}>{sentence}</p>
+                    <input className="col-6" onKeyDown={handle_key} placeholder="Click here and start Typing"/>
+                    <p className="col-6" style={{marginTop: "115px"}}>{sentence.slice(0, 20)}</p>
                 </div>                
             </div> 
 
